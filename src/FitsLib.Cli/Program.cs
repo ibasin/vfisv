4e4c2205fe1d750a13a3a3770131aee7916fa518
Ilya-Brown-LG-GRAM <ilya.basin@noblia.org>
2026-09-17 14:58:39 -0400

    progress

--- src/FitsLib.Cli/Program.cs
+++ src/FitsLib.Cli/Program.cs
@@ -8,6 +8,6 @@ public class Program
         var time = DateTime.Parse("2/15/2012 3:24:00 PM");
         PixelRegion? region = new PixelRegion(1900, 1900, 64, 64);
 
-        var images = await Driver.Get24Images(inputDir, time, region, false);
+        var images = await FitsDriver.Get24Images(inputDir, time, region, false);
     }
 }
