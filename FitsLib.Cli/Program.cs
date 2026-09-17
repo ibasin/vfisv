98f451419338187321001396612b13e602019951
Ilya-Brown-LG-GRAM <ilya.basin@noblia.org>
2026-09-17 11:51:07 -0400

    progress

--- FitsLib.Cli/Program.cs
+++ FitsLib.Cli/Program.cs
@@ -1,30 +1,13 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
-
-namespace FitsLib.Cli;
+﻿namespace FitsLib.Cli;
 
 public class Program
 {
     static async Task Main()
     {
-        var jsonOptions = new JsonSerializerOptions
-        {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            WriteIndented = true,
-            NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals
-        };
-
         var inputDir = "observation";
         var time = DateTime.Parse("2/15/2012 3:24:00 PM");
         PixelRegion? region = new PixelRegion(1900, 1900, 64, 64);
 
-        var client = new JsocClient(new HttpClient());
-        var descriptor = await client.DescribeAsync(time);
-
-        await client.DownloadSegmentsAsync(descriptor, inputDir);
-
-        File.WriteAllText(Path.Combine(inputDir, "metadata.json"), JsonSerializer.Serialize(descriptor.Metadata, jsonOptions));
-
-
+        await Driver.RunAsync(inputDir, time, region);
     }
 }
--- FitsLib/Driver.cs
+++ FitsLib/Driver.cs
@@ -1,5 +1,29 @@
-﻿namespace FitsLib;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
-public class Driver
+namespace FitsLib;
+
+public static class Driver
 {
+    public static async Task RunAsync(string inputDir, DateTime time, PixelRegion? region)
+    {
+        var jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+            NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals
+        };
+        var cancellationToken = CancellationToken.None;
+
+        var client = new JsocClient(new HttpClient());
+        var descriptor = await client.DescribeAsync(time, cancellationToken);
+
+        await client.DownloadSegmentsAsync(descriptor, inputDir, cancellationToken);
+
+        var json = JsonSerializer.Serialize(descriptor.Metadata, jsonOptions);
+        await File.WriteAllTextAsync(Path.Combine(inputDir, "metadata.json"), json, cancellationToken);
+
+
+
+    }
 }
