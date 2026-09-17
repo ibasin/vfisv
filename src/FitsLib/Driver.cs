dd904654259b77a9570b8a6d0f7da73191735f22
Ilya-Brown-LG-GRAM <ilya.basin@noblia.org>
2026-09-17 14:54:20 -0400

    progress

--- src/FitsLib/Driver.cs
+++ src/FitsLib/Driver.cs
@@ -5,7 +5,7 @@ namespace FitsLib;
 
 public static class Driver
 {
-    public static async Task<FitsImage[]> RunAsync(string inputDir, DateTime time, PixelRegion? region, bool downloadFitsFiles = true)
+    public static async Task<FitsImage[]> Get24Images(string inputDir, DateTime time, PixelRegion? region, bool downloadFitsFiles = true)
     {
         var jsonOptions = new JsonSerializerOptions
         {
