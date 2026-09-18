f1b19a2c155b9896b095ce9714fa01de19089bbd
Ilya-Brown-LG-GRAM <ilya.basin@noblia.org>
2026-09-18 18:03:07 -0400

    progress

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -2,6 +2,7 @@
 using GPULib;
 using System.Diagnostics;
 using System.Globalization;
+using Utils;
 
 namespace Cli;
 
@@ -20,20 +21,25 @@ public class Program
 
         #if DEBUG
         //validate the output images by checking a pixel value at (2000, 2000) for each of the 4 output images
-        var x = 2000;
-        var y = 2000;
-        
-        var sum = 0f;
-        for (var i = 0; i < 24; i++)
-        {
-            sum += inputImages[i][x, y];
-        }
-        
         const float tolerance = 0.0001f;
-        Debug.Assert(Math.Abs(outputImages[0][x,y] - sum) < tolerance);
-        Debug.Assert(Math.Abs(outputImages[1][x, y] - sum - 1) < tolerance);
-        Debug.Assert(Math.Abs(outputImages[2][x, y] - sum - 2) < tolerance);
-        Debug.Assert(Math.Abs(outputImages[3][x, y] - sum - 3) < tolerance);
+        using (new ConsoleTimer("Validating output images"))
+        {
+            Parallel.For(0, GpuKernel.ImgDimX, x =>
+            {
+                for (var y = 0; y < GpuKernel.ImgDimY; y++)
+                {
+                    var sum = 0f;
+                    for (var i = 0; i < 24; i++)
+                    {
+                        sum += inputImages[i][x, y];
+                    }
+                    Debug.Assert((float.IsNaN(outputImages[0][x, y]) && float.IsNaN(sum)) || Math.Abs(outputImages[0][x, y] - sum) < tolerance);
+                    Debug.Assert((float.IsNaN(outputImages[1][x, y]) && float.IsNaN(sum + 1)) || Math.Abs(outputImages[1][x, y] - sum - 1) < tolerance);
+                    Debug.Assert((float.IsNaN(outputImages[2][x, y]) && float.IsNaN(sum + 2)) || Math.Abs(outputImages[2][x, y] - sum - 2) < tolerance);
+                    Debug.Assert((float.IsNaN(outputImages[3][x, y]) && float.IsNaN(sum + 3)) || Math.Abs(outputImages[3][x, y] - sum - 3) < tolerance);
+                }
+            });
+        }
         #endif
 
         //calculate prefix and save the 4 output images as files in output directory
